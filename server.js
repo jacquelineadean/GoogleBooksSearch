41d38bd87c41196cc87b0ee9462127be4c7d615a
Jacqueline Dean <jacquelineadean@gmail.com>
2020-08-20 12:38:23 -0500

    Initializing files and React app

---
+++ server.js
@@ -0,0 +1,32 @@
+// Dependencies
+// ======================================================================================
+const express = require('express');
+
+const mongoose = require('mongoose');
+const routes = require('./routes');
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// Define middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Serve static assets
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+}
+
+// Add routes, both API and view
+// ======================================================================================
+app.use(routes);
+
+// Connection 
+// ======================================================================================
+
+// Connect to the Mongo DB
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebookssearch");
+
+// Start the API server
+app.listen(PORT, function() {
+    console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
+});
