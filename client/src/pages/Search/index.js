81d65e98a595668114905dce1da4b43be1cc3708
Jacqueline Dean <jacquelineadean@gmail.com>
2020-08-21 20:08:01 -0500

    Fixed routes and pages

--- client/src/pages/Search/index.js
+++ client/src/pages/Search/index.js
@@ -1,10 +1,113 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Input, FormBtn } from '../../components/Form';
+import { List, ListItem } from '../../components/List';
+import SaveBtn from '../../components/SaveBtn';
+import API from '../../utils/API';
 
 function Search() {
+    // Setting initial state
+    const [title, setTitle] = useState()
+    const [results, setResults] = useState([])
+    const [books, setBooks] = useState([])
+
+    // Load search results and store them 
+    useEffect(() => {
+        API.getGoogleBooks(title).then(res => setResults(res.data))
+            .catch(err => console.log(err));
+    }, [])
+
+    // function loadBooks() {
+    //     API.getBooks().then(res => setBooks(res.data)).catch(err => console.log(err));
+    // }
+
+    // Save Book
+    function saveBook(book) {
+        API.saveBook(book)
+            .then(res => {
+                const filterBooks = results.filter(book => book.id !== res.data.id);
+                setBooks({
+                    ...books,
+                    books: filterBooks
+                })
+            })
+            // loadBooks())
+            .catch(err => console.log(err));
+    }
+
+    // Handle input change
+    function handleInputChange(event) {
+        const { name, value } = event.target;
+        setTitle({
+            // ...title,
+            [name]: value
+        })
+        console.log(title);
+    }
+
+    // Handle submit
+    function handleFormSubmit(event) {
+        event.preventDefault();
+        const title = title.trim();
+        API.getBooks(title)
+            .then(res => {
+                setResults(res.data.items)
+                console.log(results)
+            })
+            .catch(err => console.log(err));
+    }
+
+
     return (
-        <div>
-            <h1>Book Search</h1>
-        </div>
+        <section>
+            <div className='container card'>
+                <h3>Book Search</h3>
+                <form className='form-group'>
+                    <p>Book Title</p>
+                    <Input
+                        name='title'
+                        value={title}
+                        onChange={handleInputChange}
+                        label='Book Title'
+                        placeholder='Search Book Title'
+                    />
+                    <FormBtn
+                        onClick={handleFormSubmit}
+                    />
+                </form>
+            </div>
+            <br></br>
+            <div className='container card'>
+                <h3>Results</h3>
+                <List>
+                    {books.map((book, index) => (
+                        <ListItem key={book.id}>
+                            <div className='card'>
+                                <img src={book.volumeInfo.imageLinks.smallThumbnail} alt='book cover' />
+                                <div className='card-body'>
+                                    <h5 className='card-title'>{book.volumeInfo.title}</h5>
+                                    <h6>{book.volumeInfo.authors}</h6>
+                                    <p className='card-text'>{book.volumeInfo.description}</p>
+                                    <a key={'' + index + book.id} href={book.volumeInfo.infoLink} target={this.state.target}>View</a>
+                                    <SaveBtn
+                                        key={'' + book.id + index}
+                                        onClick={() => saveBook({
+                                            title: book.volumeInfo.title,
+                                            author: book.volumeInfo.authors[0],
+                                            description: book.volumeInfo.description,
+                                            image: book.volumeInfo.imageLinks.smallThumbnail,
+                                            link: book.volumeInfo.infoLink,
+                                            _id: book.id
+                                        })}
+                                    >
+                                        Save
+                                    </SaveBtn>
+                                </div>
+                            </div>
+                        </ListItem>
+                    ))}
+                </List>
+            </div>
+        </section>
     )
 }
 
--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,20 +1,20 @@
 import axios from "axios";
 
 export default {
-  // Gets all books
+  // Gets all saved books
   getBooks: function() {
     return axios.get("/api/books");
   },
-  // Gets the book with the given id
-  getBook: function(id) {
-    return axios.get("/api/books/" + id);
+  // Saves a book to the database
+  saveBook: function(bookData) {
+    return axios.post("/api/books", bookData);
   },
   // Deletes the book with the given id
   deleteBook: function(id) {
     return axios.delete("/api/books/" + id);
   },
-  // Saves a book to the database
-  saveBook: function(bookData) {
-    return axios.post("/api/books", bookData);
+  // GoogleBooks API route
+  getGoogleBooks: function(title) {
+    return axios.get("https://www.googleapis.com/books/v1/volumes?q=" + title);
   }
 };
